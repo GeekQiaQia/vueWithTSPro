808a71e380f5a88e6ff48e0866f9e56447e84e7d
geekQiaQia <xiyangbai@outlook.com>
2019-10-24 14:49:38 +0800

    'update-login-test'

--- src/pages/views/login/login.vue
+++ src/pages/views/login/login.vue
@@ -1,25 +1,41 @@
 <template>
+<div>
   <div class="login-wrap">
-    {{data.pageName}}
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="用户名" prop="pass">
-        <el-input type="password" v-model="ruleForm.username" autocomplete="off"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="checkPass">
-        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-      </el-form-item>
 
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="header">
+      <a class="header-logo" href="https://cn.vuejs.org/v2/api/#key"></a>
+      <div class="header-title">
+<!--        {{data.pageName}}-->
+      <span>登录</span>
+      </div>
+    </div>
+    <div class="box">
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="loginForm">
+        <el-form-item  label="用户名" prop="username">
+          <el-input type="text" class="input326"  placeholder="请输入用户名" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item  label="密码" prop="password">
+          <el-input type="password" class="input326" placeholder="请输入密码" v-model="ruleForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button  class="btn confirm" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button  class="btn reset" @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="content-container-right">
+        <img src="../../../assets/images/word.jpg">
+    </div>
+
   </div>
+</div>
 </template>
 
 <script lang="ts" src="./login.ts"></script>
 
 <style lang="scss">
-  @import './login.scss'
+  /*@import '../../../assets/scss/common.scss';*/
+  @import './login.scss';
 </style>
 
